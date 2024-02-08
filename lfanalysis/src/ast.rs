38099d90d84ebaa8aa3b16511488bc4d08e840a1
Peter Donovan <peter@xronos.com>
2024-02-08 14:17:33 -0800

    Very rough outline of parsing the s-expressions

---
+++ lfanalysis/src/ast.rs
@@ -0,0 +1,268 @@
+use lexpr::{datum, print, Datum, Value};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Keyword {
+    Reactor,
+    Import,
+    Preamble,
+    Target,
+    Reactors,
+    Imports,
+    Preambles,
+    Instantiations,
+    IsMain,
+    IsFederated,
+    IsRealtime,
+    TodoUnrecognized,
+}
+#[derive(Debug)]
+pub struct Model {
+    target: Target,
+    imports: Vec<Import>,
+    preambles: Vec<Preamble>,
+    reactors: Vec<Reactor>,
+}
+#[derive(Debug)]
+pub struct Target {
+    // cst: Datum,
+}
+#[derive(Debug)]
+pub struct Import {
+    // cst: Datum,
+}
+#[derive(Debug)]
+pub struct Preamble {
+    // cst: Datum,
+}
+#[derive(Debug)]
+pub struct Instantiation {
+    name: String,
+}
+
+// sList(
+//     "reactor",
+//     new SAtom<>(object.getName()),
+//     fingerprint(object),
+//     sList("attributes", object.getAttributes()),
+//     sList("is-main", object.isMain()),
+//     sList("is-federated", object.isFederated()),
+//     sList("is-realtime", object.isRealtime()),
+//     sList("typeParms", object.getTypeParms()),
+//     sList("parameters", object.getParameters()),
+//     sList("host", object.getHost()),
+//     sList("extends", object.getSuperClasses()),
+//     sList("preambles", object.getPreambles()),
+//     sList("state", object.getStateVars()),
+//     sList("methods", object.getMethods()),
+//     sList("inputs", object.getInputs()),
+//     sList("outputs", object.getOutputs()),
+//     sList("timers", object.getTimers()),
+//     sList("actions", object.getActions()),
+//     sList("watchdogs", object.getWatchdogs()),
+//     sList("instantiations", object.getInstantiations()),
+//     sList("connections", object.getConnections()),
+//     sList("reactions", object.getReactions()),
+//     sList("modes", object.getModes()));
+#[derive(Debug)]
+pub struct Reactor {
+    name: String,
+    // fingerprint: [u8],
+    // attributes: Vec<Attribute>,
+    is_main: bool,
+    is_federated: bool,
+    // is_realtime: bool,
+    // type_parms: Vec<TypeParm>,
+    // parameters: Vec<Parameter>,
+    // host: String,
+    // extends: Vec<String>,
+    // preambles: Vec<Preamble>,
+    // state: Vec<StateVar>,
+    // methods: Vec<Method>,
+    // inputs: Vec<Input>,
+    // outputs: Vec<Output>,
+    // timers: Vec<Timer>,
+    // actions: Vec<Action>,
+    // watchdogs: Vec<Watchdog>,
+    instantiations: Vec<Instantiation>,
+    // connections: Vec<Connection>,
+    // reactions: Vec<Reaction>,
+    // modes: Vec<Mode>,
+}
+
+pub fn parse_model(cst: datum::Ref) -> Model {
+    let mut iter = cst.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let mut rest = iter.next().unwrap();
+    assert!(iter.next().is_none());
+    let mut iter = rest.list_iter().unwrap();
+    let checkme = iter.next().unwrap();
+    let mut next = iter.next().unwrap();
+    let mut target = parse_target(next);
+    let mut imports = Option::None;
+    let mut preambles = Option::None;
+    let mut reactors = Option::None;
+    let mut maybe_next = iter.next();
+    while let Some(next) = maybe_next {
+        let (keyword, cst) = get_field(next);
+        dbg!("DEBUG: found keyword {:?}", keyword);
+        match keyword {
+            Keyword::Reactors => {
+                assert!(reactors.is_none());
+                reactors = Some(parse_reactors(cst));
+            }
+            Keyword::Imports => {
+                assert!(imports.is_none());
+                imports = Some(parse_imports(cst));
+            }
+            Keyword::Preambles => {
+                assert!(preambles.is_none());
+                preambles = Some(parse_preambles(cst));
+            }
+            _ => panic!("unexpected keyword {:?}", keyword),
+        }
+        maybe_next = iter.next();
+    }
+    assert!(iter.next().is_none());
+    Model {
+        target,
+        imports: imports.unwrap_or_default(),
+        preambles: preambles.unwrap_or_default(),
+        reactors: reactors.unwrap_or_default(),
+    }
+}
+
+fn get_field(cst: datum::Ref) -> (Keyword, datum::Ref) {
+    let mut iter = cst.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    dbg!(cst.value().to_string());
+    let (header, rest) = iter.next().unwrap().as_pair().unwrap();
+    let mut iter = header.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let name = iter.next().unwrap();
+    assert!(iter.next().is_none());
+    let name = name.as_symbol().unwrap();
+    let k = match name {
+        "reactors" => Keyword::Reactors,
+        "imports" => Keyword::Imports,
+        "preambles" => Keyword::Preambles,
+        "reactor" => Keyword::Reactor,
+        "import" => Keyword::Import,
+        "preamble" => Keyword::Preamble,
+        "target" => Keyword::Target,
+        "instantiations" => Keyword::Instantiations,
+        "is-main" => Keyword::IsMain,
+        "is-federated" => Keyword::IsFederated,
+        "is-realtime" => Keyword::IsRealtime,
+        _ => Keyword::TodoUnrecognized,
+        // _ => panic!("unexpected symbol \"{}\"", name),
+    };
+    (k, rest)
+}
+
+fn parse_target(cst: datum::Ref) -> Target {
+    Target { /*cst: cst.into()*/ }
+}
+
+fn parse_imports(cst: datum::Ref) -> Vec<Import> {
+    cst.list_iter()
+        .unwrap()
+        .map(|cst| Import { /*cst: cst.into()*/ })
+        .collect()
+}
+
+fn parse_preambles(cst: datum::Ref) -> Vec<Preamble> {
+    cst.list_iter()
+        .unwrap()
+        .map(|cst| Preamble { /*cst: cst.into()*/ })
+        .collect()
+}
+
+fn parse_reactors(cst: datum::Ref) -> Vec<Reactor> {
+    cst.list_iter()
+        .unwrap()
+        .map(|cst| parse_reactor(cst))
+        .collect()
+}
+
+fn parse_reactor(cst: datum::Ref) -> Reactor {
+    let mut iter = cst.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let mut iter = iter.next().unwrap().list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let name = iter.next().unwrap();
+    let fingerprint = iter.next().unwrap();
+    dbg!(fingerprint.value().to_string());
+    let mut instantiations = Option::None;
+    let mut is_main = Option::None;
+    let mut is_federated = Option::None;
+    while let Some(next) = iter.next() {
+        let (keyword, cst) = get_field(next);
+        match keyword {
+            Keyword::IsMain => {
+                assert!(is_main.is_none());
+                is_main = Some(parse_bool(cst));
+            }
+            Keyword::IsFederated => {
+                assert!(is_federated.is_none());
+                is_federated = Some(parse_bool(cst));
+            }
+            Keyword::Instantiations => {
+                assert!(instantiations.is_none());
+                instantiations = Some(parse_instantiations(cst));
+            }
+            _ => {
+                dbg!(keyword);
+            } // _ => panic!("unexpected keyword {:?}", keyword),
+        }
+    }
+    dbg!(name.value().to_string());
+    let mut iter = name.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let name = iter.next().unwrap();
+    dbg!(name.value());
+    let name = name.as_str().unwrap();
+    // let fingerprint = fingerprint.as_bytes().unwrap();
+    Reactor {
+        name: name.to_string(),
+        instantiations: instantiations.unwrap_or_default(),
+        is_main: is_main.unwrap_or(false),
+        is_federated: is_federated.unwrap_or(false),
+    }
+}
+
+fn parse_instantiations(cst: datum::Ref) -> Vec<Instantiation> {
+    cst.list_iter()
+        .unwrap()
+        .map(|cst| parse_instantiation(cst))
+        .collect()
+}
+
+fn parse_instantiation(cst: datum::Ref) -> Instantiation {
+    let mut iter = cst.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let name = iter.next().unwrap();
+    let mut iter = name.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let name = iter.next().unwrap();
+    let mut iter = name.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let name = iter.next().unwrap();
+    // assert!(iter.next().is_none()); // FIXME
+    dbg!(name.value().to_string());
+    let name = name.value().as_str().unwrap();
+    Instantiation {
+        name: name.to_string(),
+    }
+}
+
+fn parse_bool(cst: datum::Ref) -> bool {
+    let mut iter = cst.list_iter().unwrap();
+    let unboxed = iter.next().unwrap();
+    assert!(iter.next().is_none());
+    let mut iter = unboxed.list_iter().unwrap();
+    let ogspan = iter.next().unwrap();
+    let value = iter.next().unwrap();
+    dbg!(value.value());
+    assert!(iter.next().is_none());
+    value.as_bool().unwrap()
+}
--- lfanalysis/src/lib.rs
+++ lfanalysis/src/lib.rs
@@ -1,4 +1,7 @@
+mod ast;
+
 pub fn federate_names(lf_file: String) -> Vec<String> {
+    let cst = lexpr::datum::from_str(&lf_file).unwrap();
     vec![
         String::from("federate1"),
         String::from("federate2"),
@@ -8,34 +11,160 @@ pub fn federate_names(lf_file: String) -> Vec<String> {
 
 #[cfg(test)]
 mod tests {
+    use self::ast::parse_model;
+
     use super::*;
 
     #[test]
     fn temp() {
-        let expected = expect_test::expect![[
-            r#"Cons((Cons((Symbol("name") . String("John Doe"))) . Cons((Cons((Symbol("age") . Number(PosInt(43)))) . Cons((Cons((Symbol("address") . Cons((Cons((Symbol("street") . Cons((String("10 Downing Street") . Null)))) . Cons((Cons((Symbol("city") . Cons((String("London") . Null)))) . Null)))))) . Cons((Cons((Symbol("phones") . Cons((String("+44 1234567") . Cons((String("+44 2345678") . Null)))))) . Null))))))))"#
-        ]];
-        expected.assert_eq(&format!(
-            "{:?}",
-            lexpr::from_str(
-                r#"
-        ((name . "John Doe")
-            (age . 43)
-            (address
-            (street "10 Downing Street")
-            (city "London"))
-            (phones "+44 1234567" "+44 2345678"))
-        "#,
-            )
-            .unwrap()
-        ));
-    }
-    #[test]
-    fn temp2() {
         let expected = expect_test::expect![[r#""#]];
         expected.assert_eq(&format!(
             "{:?}",
-            lexpr::from_str(r#"((a . 42) (b . 43))"#).unwrap()
+            parse_model(
+                lexpr::datum::from_str(
+                    r#"
+(("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 0 0 0 23 0)
+ ((() model)
+  (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 0 0 0 13 1)
+   ((() target-decl)
+    (() "C")
+    (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 303 6 8 13 1)
+     ((() key-value-pairs)
+      (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 305 6 10 7 16)
+       ((() pair)
+        (() "timeout")
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 316 7 10 7 16)
+         ((() element)
+          (() ((() time) (() 5) (() "sec")))))))
+      (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 323 7 17 8 16)
+       ((() pair)
+        (() "logging")
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 334 8 10 8 16)
+         ((() element) (() "DEBUG")))))
+      (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 341 8 17 9 27)
+       ((() pair)
+        (() "coordination")
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 357 9 15 9 27)
+         ((() element) (() "centralized")))))
+      (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 370 9 28 12 3)
+       ((() pair)
+        (() "docker")
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 380 10 9 12 3)
+         ((() element)
+          (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 380 10 9 12 3)
+           ((() key-value-pairs)
+            (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 382 10 11 11 26)
+             ((() pair)
+              (() "rti-image")
+              (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 397 11 14 11 26)
+               ((() element) (() "🂠rti:local🂠")))))))))))))))
+  (() ((() imports)
+    (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 415 13 1 15 38)
+     ((() import)
+      (() "../../lib/Count.lf")
+      (() ((() reactors)
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 423 15 6 15 12)
+         ((() imported-reactor) (() #u8(122 167 207 110 254 203 222 200 147 255 116 189 67 218 241 149 145 54 174 0)) (() "Count") (() #u8(20 44 36 137 94 248 108 19 12 187 254 130 16 52 135 123 86 159 213 99))))))))
+    (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 455 15 38 16 55)
+     ((() import)
+      (() "../../federated/DistributedCount.lf")
+      (() ((() reactors)
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 462 16 6 16 12)
+         ((() imported-reactor) (() #u8(157 122 104 62 132 139 183 223 93 138 177 86 223 18 127 91 242 7 134 124)) (() "Print") (() #u8(246 135 206 84 110 36 57 249 140 57 242 119 177 224 255 62 225 82 225 216))))))))))
+  (() ((() reactors)
+    (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 511 16 55 22 1)
+     ((() reactor)
+      (() "DistributedCountContainerized")
+      (() #u8(128 73 67 242 145 70 215 14 196 33 71 74 252 6 63 123 189 48 232 58))
+      (() ((() is-main) (() #f)))
+      (() ((() is-federated) (() #t)))
+      (() ((() is-realtime) (() #f)))
+      (() ((() parameters)
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 561 18 48 18 71)
+         ((() parameter)
+          (() "offset")
+          (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 568 18 55 18 60)
+           ((() type)
+            (() ((() stars) (() 0)))
+            (() ((() is-time) (() #t)))))
+          (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 573 18 60 18 71)
+           ((() initializer)
+            (() ((() exprs)
+              (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 575 18 62 18 71)
+               ((() time) (() 200) (() "msec")))))
+            (() ((() is-braces) (() #f)))
+            (() ((() is-parens) (() #f)))
+            (() ((() is-assign) (() #t)))
+            (() ((() is-trailing-comma) (() #f)))))))))
+      (() ((() host)))
+      (() ((() instantiations)
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 587 18 74 19 17)
+         ((() instantiation)
+          (() "c")
+          (() ((() reactor) (() "Count") (() #u8(20 44 36 137 94 248 108 19 12 187 254 130 16 52 135 123 86 159 213 99))))))
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 605 19 17 20 17)
+         ((() instantiation)
+          (() "p")
+          (() ((() reactor) (() "Print") (() #u8(246 135 206 84 110 36 57 249 140 57 242 119 177 224 255 62 225 82 225 216))))))))
+      (() ((() connections)
+        (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 623 20 17 21 28)
+         ((() connection)
+          (() ((() left-ports)
+            (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 623 20 17 21 7)
+             ((() var-ref)
+              (("/home/peter/lingua-franca/test/C/src/lib/Count.lf" 88 3 22 4 17)
+               ((() output) (() "out") (() #u8(149 60 206 233 93 145 46 121 86 217 10 202 191 203 43 215 224 138 163 243))))
+              (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 587 18 74 19 17)
+               ((() instantiation)
+                (() "c")
+                (() ((() reactor) (() "Count") (() #u8(20 44 36 137 94 248 108 19 12 187 254 130 16 52 135 123 86 159 213 99))))))
+              (() ((() is-interleaved) (() #f)))
+              (() ((() alias)))))))
+          (() ((() right-ports)
+            (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 634 21 10 21 15)
+             ((() var-ref)
+              (("/home/peter/lingua-franca/test/C/src/federated/DistributedCount.lf" 407 13 15 14 15)
+               ((() input) (() "in") (() #u8(244 164 38 91 225 219 148 206 69 62 232 31 141 96 7 92 6 102 125 61))))
+              (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 605 19 17 20 17)
+               ((() instantiation)
+                (() "p")
+                (() ((() reactor) (() "Print") (() #u8(246 135 206 84 110 36 57 249 140 57 242 119 177 224 255 62 225 82 225 216))))))
+              (() ((() is-interleaved) (() #f)))
+              (() ((() alias)))))))
+          (() ((() is-iterated) (() #f)))
+          (() ((() is-physical) (() #f)))
+          (() ((() delay)
+            (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 645 21 21 21 28)
+             ((() parameter-reference)
+              (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 561 18 48 18 71)
+               ((() parameter)
+                (() "offset")
+                (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 568 18 55 18 60)
+                 ((() type)
+                  (() ((() stars) (() 0)))
+                  (() ((() is-time) (() #t)))))
+                (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 573 18 60 18 71)
+                 ((() initializer)
+                  (() ((() exprs)
+                    (("/home/peter/lingua-franca/test/C/src/docker/federated/DistributedCountContainerized.lf" 575 18 62 18 71)
+                     ((() time) (() 200) (() "msec")))))
+                  (() ((() is-braces) (() #f)))
+                  (() ((() is-parens) (() #f)))
+                  (() ((() is-assign) (() #t)))
+                  (() ((() is-trailing-comma) (() #f)))))))))))))))))))))
+            "#
+                )
+                .unwrap()
+                .as_ref()
+            )
         ));
     }
+    // #[test]
+    // fn temp2() {
+    //     let expected = expect_test::expect![[r#""#]];
+    //     expected.assert_eq(&format!(
+    //         "{:?}",
+    //         lexpr::from_str(r#"((a . 42) (b . 43))"#).unwrap()
+    //     ));
+    // }
 }
